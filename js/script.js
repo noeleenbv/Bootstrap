b9482bce2b4ea223ccdcd0aa11219ef6dda1594f
Noeleen Vivian <noeleenbv@gmail.com>
2016-08-11 16:17:19 +0100

    Content containers and Javascript components - tutorial excercise files.

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(function() {
+
+	$('.carousel').carousel({
+		interval: 2000,
+		pause: false,
+		wrap: false,
+		keyboard: false
+	});
+
+});
+
+$(function() {
+
+	$('[data-toggle="tooltip"]').tooltip();
+
+});
+
+$(function() {
+
+	$('[data-toggle="popover"]').popover();
+
+});
